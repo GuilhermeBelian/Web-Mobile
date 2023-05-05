086480bcbaa63a42c8c12d13c6343888ce740620
GuilhermeBelian <guilhermebelian2003@gmail.com>
2023-05-05 11:24:26 +0000

    Atividade 5 Fetch API

---
+++ Atv 5/script.js
@@ -0,0 +1,10 @@
+function getRandomDog() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        const dogImage = document.getElementById('dog-image');
+        dogImage.src = data.message;
+      })
+      .catch(error => console.error(error));
+  }
+  getRandomDog();
